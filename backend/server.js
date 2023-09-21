7b07c16c001b9f980e730a4992cbc7fb555f6247
pmesparas <paulmesparas@gmail.com>
2023-09-21 14:37:44 +0800

    without authentication

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//variables
+const port = process.env.PORT;
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to the app" });
+// });
+app.use("/api/workouts", workoutRoutes);
+
+//connect to database (async)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen
+    app.listen(port, () =>
+      console.log(`Connected to DB and Listening to port ${port}`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
